7a65ecd9a01e1cb7f9f5814834a7a2b07bd4a522
Eduardo C. <javaphpscala@gmail.com>
2026-01-25 23:13:38 -0500

    first commit

---
+++ src/main/java/com/eduardos/ecommerce_2026/Ecommerce2026Application.java
@@ -0,0 +1,13 @@
+package com.eduardos.ecommerce_2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ecommerce2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ecommerce2026Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/eduardos/ecommerce_2026/Ecommerce2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardos.ecommerce_2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ecommerce2026ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
